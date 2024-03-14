520a132b6beeca705b7baef05fe741965c368ab1
kiquetal <kiquetal@gmail.com>
2024-03-14 10:42:47 -0300

    just adding profile

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ NODE_LAMBDA=./node_modules/node-lambda/bin/node-lambda
 test -s $NODE_LAMBDA || { echo "node-lambda not installed. Run 'npm install' first."; exit 1; }
 mkdir -p tmp
 cat .env | grep HOOK_URL > ./tmp/deploy.env
-$NODE_LAMBDA deploy --configFile ./tmp/deploy.env
+$NODE_LAMBDA deploy --configFile ./tmp/deploy.env --profile investor-prod --runtime nodejs18.x
